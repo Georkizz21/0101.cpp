243bf7de160a2108e1af76847305cd0c2da67ad2
Georkizz21 <tdoanthu2@gmail.com>
2026-03-03 16:50:51 +0700

    0115.cpp

--- 0115.cpp
+++ 0115.cpp
@@ -1,24 +1,47 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std ;
 
-int main (){
-	int t;
-	cin >> t;
+bool checkNT(int n){
 	
-	while (t--){
-		int n;
-		cin >> n ;
-		int k=n;
-		for(int i = 2 ; i*i <= k; i++ ){
-			int num=0 ;
-			while ( n% i == 0 ){
-				num++;
-				n=n/i;
-			}
-			if (num >=1)cout << i << " " << num << " " ;
-		}
+	for(int i = 2 ; i * i <= n ; i++){
+		if( n % i == 0 ) return false ; 
+	} 
+	return true ; 
+	
+}
+
+int main(){
+	
+	int t ;
+	cin >> t ;
+	
+	while(t--){
+	
+	int n ;
+	cin >> n ;
+	
+	bool check = checkNT( n );
+	
+	for( int i = 2 ; i * i  <= n ; i++ ){
 		
-		if(n>1  )cout << n << " " << 1 << " "<< endl;
-		else cout << endl;
+		int count = 0 ;
+		
+		if(checkNT(i) && n % i == 0){
+		
+			while(n%i== 0){
+			
+				n=n/i;
+				count++;
+				
+			}
+			cout << i <<" " << count <<" " ;
+		} 
 	}
+
+	if( n > 1 ) cout << n <<" " << 1 ; 
+	
+	cout << endl ;
+}
+	return 0 ;
+	
 }
